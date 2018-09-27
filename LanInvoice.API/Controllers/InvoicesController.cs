22fa4d4b8578f9d61a8aa03216ced0dbdd714273
Esa Hämölä <epituo@gmail.com>
2018-09-27 18:46:28 +0300

    Added InvoicesController

---
+++ LanInvoice.API/Controllers/InvoicesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanInvoice.API.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        public JsonResult GetInvoices()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id = 1, Number="60"}, 
+                new { id = 2, Number="61"}
+            });
+        }
+    }
+}
